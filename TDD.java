d1bbd94e0c0f0f62f033cf14b8e362dc25ad6dd2
alejandro baena perez <alejandro baena perez>
2016-05-19 16:48:20 +0200

    creado TODO TDD.java

--- TDD.java
+++ TDD.java
@@ -1,13 +1,52 @@
 import java.util.Hashtable;
 
 public class TDD {
-private Hashtable <String, String> tabla = new Hashtable <String, String>();
+	private Hashtable<String, String> tabla = new Hashtable<String, String>();
 
-	public void put (String clave, String valor){
-		if(tabla.contains(clave)){
+	public void put(String clave, String valor) {
+		if (tabla.contains(clave)) {
 			tabla.replace(clave, valor);
 		} else {
 			tabla.put(clave, valor);
 		}
+	}
+
+	public String get(String clave) {
+		String dar = "";
+
+		if (tabla.containsKey(clave)) {
+			dar = tabla.get(clave);
+		} else {
+			throw new TDDException(tabla.toString());
+		}
+		return dar;
+	}
+	public String getOrElse (String clave, String valorPorDefecto){
+		String dar = "";
+
+		if (tabla.containsKey(clave)) {
+			dar = tabla.get(clave);
+		} else {
+			dar = tabla.get(valorPorDefecto);
+		}
+		return dar;
+	}
+	public boolean containsKey (String clave){
+		boolean esta = false;
+		if(tabla.containsKey(clave) == esta){
+			esta = true;
+		}
+		return esta;
+	}
+	public boolean remove (String clave){
+		boolean exito = false;
+		if(tabla.containsKey(clave)){
+			tabla.remove(clave);
+			exito = true;
+		}
+		return exito;
+	}
+	public int size(){
+		return tabla.size();
 	}
 }
