84d00158f7372529aa36f8f63a822809e4994822
alejandro baena perez <alejandro baena perez>
2016-05-19 16:29:38 +0200

    creado el put

--- TDD.java
+++ TDD.java
@@ -1,4 +1,13 @@
+import java.util.Hashtable;
 
 public class TDD {
+private Hashtable <String, String> tabla = new Hashtable <String, String>();
 
+	public void put (String clave, String valor){
+		if(tabla.contains(clave)){
+			tabla.replace(clave, valor);
+		} else {
+			tabla.put(clave, valor);
+		}
+	}
 }
